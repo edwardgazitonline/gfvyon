4c77a3f36d39bc4c05365c0cc48f640d9e97b895
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2024-05-01 02:03:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gfvyon
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/edwardgazitonline/gfvyon)
---
+++ src/app/component-overview/component-overview.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+// Your component path
+import {ComponentOverviewComponent} from './component-overview.component';
+
+@NgModule({
+  declarations: [
+    // Existing Components
+   ComponentOverviewComponent
+  ]
+  // Rest of the Module Content
+})
+export class ComponentOverviewModule {}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
